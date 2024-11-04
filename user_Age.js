c0ffd57a0db99860617f6d2e7adc874434ba9a24
pauuulaaa <heleinapaula@gmail.com>
2024-11-04 19:30:41 +0800

    initial commit

---
+++ user_Age.js
@@ -0,0 +1,15 @@
+let age = 19;
+console.log(age)
+
+if (age <= 5)
+    console.log("toddler");
+else if (age >= 6 & age <= 12)
+    console.log("Child");
+else if (age >= 13 & age <= 19)
+    console.log("teenager");
+else if (age <= 20 & age >= 35)
+    console.log("Young Adult");
+else if (age <= 36 & age >= 60)
+    console.log("Middle Aged");
+else
+    console.log("Senior")
